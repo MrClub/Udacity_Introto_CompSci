fe9719a679d1781d6b7a78bd796696d459442708
MrClub <40201366+MrClub@users.noreply.github.com>
2018-06-14 12:37:36 +0100

    Updated readme

--- Basic Link Extraction.py
+++ Basic Link Extraction.py
@@ -12,17 +12,17 @@
 
 # page = contents of a web page
 page = ('<div id="top_bin"><a href="http://github.com"><div id="top_content" class="width960">'
-'<div class="udacity float-left"><a href="http://udacity.com">')
+        '<div class="udacity float-left"><a href="http://udacity.com">')
 start_link = page.find('<a href=')
 
 start_quote = page.find('"', start_link)
 end_quote = page.find('"', start_quote + 1)
-url1 = page[start_quote + 1 :end_quote]
-second_link = page.find('<a href=', start_link +1)
+url1 = page[start_quote + 1:end_quote]
+second_link = page.find('<a href=', start_link + 1)
 second_quote = page.find('"', second_link)
 second_end_quote = page.find('"', second_quote + 1)
-url2 = page[second_quote+1:second_end_quote]
+url2 = page[second_quote + 1:second_end_quote]
 print(url1)
 print(url2)
 
-#this is a test
+# this is a test
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Udacity_Introto_CompSci
+I'm doing the Udacity Introduction to computer Science course. This reposititory is for the quizzes, homework and just general tests with that course.
